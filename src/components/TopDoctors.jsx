ce7eeecb86012f82177790a188f27b16493c3ced
usamakhan-90 <us.usamashahid@gmail.com>
2025-07-16 17:27:52 +0500

    Implement AppContext for managing doctors data and update TopDoctors component for better UI and navigation

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import { doctors } from '../assets/assets'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 function TopDoctors() {
-  return (
-    <>
-    <div className='flex flex-col items-center gap-4 py-16 text-gray-900 md:px-10'>
-        <h1>Top Doctors to Book</h1>
-        <p>Simply browse through our extensive list of trusted doctors.</p>
-        <div>
-            {
-                doctors.slice(0, 10).map((doctor, index)=>(
-                    <div>
-                        <img src={doctor.image} alt="" />
-                        <div>
-                            <p></p><p>Available</p>
-                        </div>
-                        <p>{doctor.name}</p>
-                        <p>{doctor.speciality}</p>
-                    </div>
-                ))
-            }
-        </div>
-        <button>more</button>
-    </div>
-    </>
-  )
+
+    const {doctors} = useContext(AppContext);
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className='flex flex-col items-center gap-4 py-16 text-gray-900 md:px-10'>
+                <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+                <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+                <div className='w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 gap-y-6 pt-5 px-3 sm:px-0'>
+                    {
+                        doctors.slice(0, 10).map((doctor, index) => (
+                            <div onClick={()=>navigate(`/appointement/${doctor._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                                <img className='bg-blue-50' src={doctor.image} alt="" />
+                                <div className='p-4'>
+                                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                                        <p className='size-2 bg-green-500 rounded-full'></p><p>Available</p>
+                                    </div>
+                                    <p className='text-gray-900 text-lg font-medium'>{doctor.name}</p>
+                                    <p className='text-gray-600 text-sm'>{doctor.speciality}</p>
+                                </div>
+
+                            </div>
+                        ))
+                    }
+                </div>
+                <button onClick={()=>{navigate("/doctors"),scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+            </div>
+        </>
+    )
 }
 
 export default TopDoctors
